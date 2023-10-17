b0d7adb390105999faddf19169b143686986b28a
Martin Wimpress <martin@wimpress.org>
2023-10-17 19:47:17 +0100

    fix: add missing obs_register_source()

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -172,6 +172,7 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 bool obs_module_load(void)
 {
+	obs_register_source(&rgb_levels_filter);
 	obs_log(LOG_INFO, "plugin loaded successfully (version %s)",
 		PLUGIN_VERSION);
 	return true;
